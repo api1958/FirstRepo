cd7cbdc3cb5dfd692a242723ff602fc364432d37
Amandeep <amandeep.m.a@apisero.com>
2021-09-28 16:36:17 +0530

    First commit

--- Simple.java
+++ Simple.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
+     System.out.println("How are you"); 
      System.out.println("Bye");
     }  
 }  
